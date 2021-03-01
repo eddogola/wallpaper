46a35f53c35bc395dc7be6745d6aaacf167baf9d
Ogola Eddy <eddogola01@gmail.com>
2021-03-01 10:08:51 +0300

    client http methods

--- client.go
+++ client.go
@@ -1,6 +1,9 @@
 package wallpaper
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -20,4 +23,46 @@ type Config struct {
 // NewClient creates a new Client with provided authentication keys
 func NewClient(auth *UnsplashAuth) *Client {
 	return &Client{Auth: auth}
+}
+
+func (c *Client) getHTTP(ctx context.Context, link string) (*http.Response, error) {
+	client := c.HTTPClient
+	if client == nil {
+		client = http.DefaultClient
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, link, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating request")
+	}
+
+	authorizationHeaderValue := fmt.Sprintf("Client-ID %s", c.Auth.AccessKey)
+	req.Header.Set("Authorization", authorizationHeaderValue)
+	queryVars := req.URL.Query()
+	queryVars.Add("per_page", fmt.Sprint(c.Config.ItemsPerPage))
+	req.URL.RawQuery = queryVars.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making request %v", req)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errUnexpectedStatusCode(resp.StatusCode)
+	}
+	return resp, nil
+}
+
+func (c *Client) getHTTPBodyBytes(ctx context.Context, link string) ([]byte, error) {
+	resp, err := c.getHTTP(ctx, link)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
--- errors.go
+++ errors.go
@@ -2,9 +2,16 @@ package wallpaper
 
 import (
 	"errors"
+	"fmt"
 )
 
 var (
 	errKeysNotInEnvVars = errors.New("authentication keys not found in os env")
 	errKeysNotInEnvFile = errors.New("authentication keys not found in provided .env file")
 )
+
+type errUnexpectedStatusCode int
+
+func (err errUnexpectedStatusCode) Error() string {
+	return fmt.Sprintf("got unexpected status code: %d", err)
+}
