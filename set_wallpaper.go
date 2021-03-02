e08fce8c24a451faed614029ebe4a9b917b7a39f
Ogola Eddy <eddogola01@gmail.com>
2021-03-02 11:51:52 +0300

    created go file that should set desktop wallpaper

---
+++ set_wallpaper.go
@@ -0,0 +1,12 @@
+package wallpapr
+
+import (
+	"os/exec"
+)
+
+/*
+set desktop background using gsettings
+
+$ gsettings set org.gnome.desktop.background picture-uri 'file:///home/JohnDoe/Pictures/wallpaper.jpg'
+
+*/
