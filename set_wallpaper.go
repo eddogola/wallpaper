208cd19f75fcffd0e06c1b9a239cbf0cb90dd230
Ogola Eddy <eddogola01@gmail.com>
2021-03-03 16:52:02 +0300

    sets desktop background

--- set_wallpaper.go
+++ set_wallpaper.go
@@ -1,6 +1,7 @@
 package wallpapr
 
 import (
+	"fmt"
 	"os/exec"
 )
 
@@ -10,3 +11,15 @@ set desktop background using gsettings
 $ gsettings set org.gnome.desktop.background picture-uri 'file:///home/JohnDoe/Pictures/wallpaper.jpg'
 
 */
+
+// SetWallpaper uses gsettings to set background pic
+func SetWallpaper(filePath string) error {
+	file := fmt.Sprintf("file://%v", filePath)
+	gsettingsCmd := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", file)
+
+	if err := gsettingsCmd.Run(); err != nil {
+		return fmt.Errorf("please check your installation contains gsettings, err: %w", err)
+	}
+
+	return nil
+}
