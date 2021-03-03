2d4ea8a3098480162f9536551de97f8d2e0d5a19
Ogola Eddy <eddogola01@gmail.com>
2021-03-03 17:04:53 +0300

    batch downloads requested pics

--- cmd/wallpapr/set.go
+++ cmd/wallpapr/set.go
@@ -1,35 +1,71 @@
 package main
 
 import (
+	"os"
+	"strings"
+
+	"github.com/eddogola/wallpapr"
 	"github.com/spf13/cobra"
 )
 
 var setCmd = &cobra.Command{
-	Use: "set",
-	Short: "Sets the desktop background",
+	Use:     "set",
+	Short:   "Sets the desktop background",
 	Example: "wallpapr set --search food --freq mins --time 15",
 	// Args: cobra.exactArgs(1),
-	Run: func(cmd*cobra.Command, args []string) {
-		// stuff
+	Run: func(cmd *cobra.Command, args []string) {
+		setWallpaper()
 	},
 }
 
 var (
-	all bool
+	all      bool
 	userName string
-	topic string
-	search string
-	freq string
-	time float64
+	topic    string
+	search   string
+	freq     string
+	time     float64
 )
 
 func init() {
 	rootCmd.AddCommand(setCmd)
-	
+
 	setCmd.Flags().BoolVarP(&all, "all", "a", true, "Get photos from a list of all Unsplash photos")
 	setCmd.Flags().StringVarP(&userName, "username", "u", "", "The username of an Unsplash user, from whom to get wallpapers.")
 	setCmd.Flags().StringVarP(&topic, "topic", "t", "", "The topic from which to get photos from unsplash.")
 	setCmd.Flags().StringVarP(&search, "search", "s", "", "The search query whose results will provide wallpapers.")
 	setCmd.Flags().StringVarP(&freq, "freq", "f", "hours", "The frequency with which to change the wallpaper(mins, hours, days). Default is `hours`.")
 	setCmd.Flags().Float64Var(&time, "time", 20, "The time after which the wallpaper should change, in the given frequency. For example, --freq mins --time 10: the wallpaper will change every 10 minutes.")
+}
+
+func setWallpaper() {
+	var photos []wallpapr.Photo
+	if all {
+		pics, err := client.GetFirstPagePhotos()
+		exitOnError(err)
+		photos = append(photos, pics...)
+	}
+	if userName != "" {
+		pics, err := client.GetPhotosByUser(userName)
+		exitOnError(err)
+		photos = append(photos, pics...)
+	}
+	if topic != "" {
+		pics, err := client.GetPhotosByTopic(topic)
+		exitOnError(err)
+		photos = append(photos, pics...)
+	}
+	if search != "" {
+		pics, err := client.GetPhotosBySearchQuery(search)
+		exitOnError(err)
+		photos = append(photos, pics...)
+	}
+
+	homeDir, err := getHomeDir()
+	exitOnError(err)
+	defaultDlLocation := homeDir + string(os.PathSeparator) + strings.Join([]string{"Downloads", "wallpapers"}, string(os.PathSeparator))
+	
+	for _, photo := range photos {
+		photo.Download(defaultDlLocation)
+	}
 }
