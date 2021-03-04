ba9d0e6d5d650fa2585074a8ab0d3db722594a39
Ogola Eddy <eddogola01@gmail.com>
2021-03-04 11:29:54 +0300

    slideshow frequency

--- cmd/wallpapr/set.go
+++ cmd/wallpapr/set.go
@@ -3,14 +3,15 @@ package main
 import (
 	"os"
 	"strings"
+	"time"
 
 	"github.com/eddogola/wallpapr"
 	"github.com/spf13/cobra"
 )
 
 var setCmd = &cobra.Command{
-	Use:     "set",
-	Short:   "Sets the desktop background",
+	Use:   "set",
+	Short: "Sets the desktop background",
 	Long: `Batch downloads several photos requested by the user(by search query, topic or Unsplash user).
 Starts a slideshow of photos in the photos' storage directory, with the photos changing after the specified
 duration`,
@@ -22,12 +23,12 @@ duration`,
 }
 
 var (
-	all      bool
-	userName string
-	topic    string
-	search   string
-	freq     string
-	time     float64
+	all       bool
+	userName  string
+	topic     string
+	search    string
+	freq      string
+	timeTaken float64
 )
 
 func init() {
@@ -37,8 +38,8 @@ func init() {
 	setCmd.Flags().StringVarP(&userName, "username", "u", "", "The username of an Unsplash user, from whom to get wallpapers.")
 	setCmd.Flags().StringVarP(&topic, "topic", "t", "", "The topic from which to get photos from unsplash.")
 	setCmd.Flags().StringVarP(&search, "search", "s", "", "The search query whose results will provide wallpapers.")
-	setCmd.Flags().StringVarP(&freq, "freq", "f", "hours", "The frequency with which to change the wallpaper(mins, hours, days). Default is `hours`.")
-	setCmd.Flags().Float64Var(&time, "time", 20, "The time after which the wallpaper should change, in the given frequency. For example, --freq mins --time 10: the wallpaper will change every 10 minutes.")
+	setCmd.Flags().StringVarP(&freq, "freq", "f", "hours", "The frequency with which to change the wallpaper(mins, hours). Default is `hours`.")
+	setCmd.Flags().Float64Var(&timeTaken, "time", 20, "The time after which the wallpaper should change, in the given frequency. For example, --freq mins --time 10: the wallpaper will change every 10 minutes.")
 }
 
 func setWallpaper() {
@@ -67,8 +68,21 @@ func setWallpaper() {
 	homeDir, err := getHomeDir()
 	exitOnError(err)
 	defaultDlLocation := homeDir + string(os.PathSeparator) + strings.Join([]string{"Downloads", "wallpapers"}, string(os.PathSeparator))
-	
+
+	var frequency time.Duration
+
+	switch freq {
+	case "mins":
+		frequency = time.Minute * time.Duration(timeTaken)
+	case "hours":
+		frequency = time.Hour * time.Duration(timeTaken)
+	}
+
+	// download all gotten photos
 	for _, photo := range photos {
 		photo.Download(defaultDlLocation)
 	}
+
+	// set up slideshow
+	
 }
